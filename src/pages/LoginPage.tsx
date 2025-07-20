d258b4823d26dcc451f3404923ef097592a12932
Kenneth Nwoye <knwy@ymail.com>
2025-07-20 15:14:27 -0700

    email bug fix

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -33,7 +33,7 @@ const LoginPage = () => {
       const detail = rest.join(":").trim();
 
       if (statusCode === "403" && detail === "Email not verified") {
-        setError("Email not verified. Please check your inbox.");
+        setError("Email not verified. Please check your inbox or span folder.");
       } else if (statusCode === "401" && detail === "Invalid credentials") {
         setError("Invalid username or password.");
       } else {
